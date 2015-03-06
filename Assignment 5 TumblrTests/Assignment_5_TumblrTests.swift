dc6e2b8bfa5b9857454c9ad9f4d6deeb894fba86
Patrick Wong <patrickwong@Patbookpro.local>
2015-03-05 22:27:50 -0800

    Initial Commit

---
+++ Assignment 5 TumblrTests/Assignment_5_TumblrTests.swift
@@ -0,0 +1,36 @@
+//
+//  Assignment_5_TumblrTests.swift
+//  Assignment 5 TumblrTests
+//
+//  Created by Patrick Wong on 3/5/15.
+//  Copyright (c) 2015 Patrick Wong. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Assignment_5_TumblrTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
